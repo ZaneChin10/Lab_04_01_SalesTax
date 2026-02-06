5e5a72610c3b488c258f88eb1a22275108ab02e4
Zane Sawyer <sawyerzc@mail.uc.edu>
2026-02-06 09:45:15 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gut
+public class Main {
+    public static void main(String[] args) {
+
+        
+        int myFavNum = 23;
+        int doubleNum = 0;
+
+        doubleNum = myFavNum * 2;
+
+        System.out.println("The number " + myFavNum + " doubled is " + doubleNum);
+
+    }
+}
